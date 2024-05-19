fd620251a5c87283ba654968ae2aefdf53040583
aravindjey <aravindjayaseelan07@gmail.com>
2024-05-20 00:08:04 +0530

    1-ui&webhooks

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authconfig = {
+  
+    providers: [
+      {
+        domain: "https://assured-mollusk-89.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+ };
+  
+export default authconfig;
