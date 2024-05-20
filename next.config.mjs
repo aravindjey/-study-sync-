56b7e47b3ff31fb6d6b691641c7c2f6106051b78
aravindjey <aravindjayaseelan07@gmail.com>
2024-05-20 10:29:56 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "spotted-dragon-778.convex.cloud" },
+			{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+		],
+	},
+};
 
 export default nextConfig;
+
